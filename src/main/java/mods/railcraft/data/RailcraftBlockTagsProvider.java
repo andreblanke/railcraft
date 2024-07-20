70cdee76df2967afc8ba0db74e7d935f33ad9123
Davide Albiero <davide2910@live.it>
2024-07-20 13:57:28 +0200

    Update RailcraftBlockTagsProvider.java

--- src/main/java/mods/railcraft/data/RailcraftBlockTagsProvider.java
+++ src/main/java/mods/railcraft/data/RailcraftBlockTagsProvider.java
@@ -6,7 +6,6 @@ import mods.railcraft.tags.RailcraftTags;
 import mods.railcraft.world.level.block.RailcraftBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.common.Tags;
